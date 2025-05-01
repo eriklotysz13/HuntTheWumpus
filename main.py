125b28a0f4e837a4d9de60a5f4212ec1d99a15b4
Erik Lotysz <lotysze13@mycu.concord.edu>
2025-05-01 16:19:32 -0400

    added hazards

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import random
 
+
 class Cave:
+    def __init__(self):
+        self.rooms = self.generate_cave()
+        self.wumpus = random.randint(0, 19)
+        self.pits = random.sample([i for i in range(20) if i != self.wumpus], 2)
+        self.bats = random.sample([i for i in range(20) if i not in self.pits and i != self.wumpus], 2)
+        self.player = random.choice([i for i in range(20) if i not in self.pits and i != self.wumpus])
+        self.alive = True
+
     def generate_cave(self):
-        # Each room has 3 unique connections, creating a basic map
+        # Each room has 3 unique connections
         cave = {}
         for i in range(20):
             connections = set()
@@ -12,3 +21,15 @@ class Cave:
                     connections.add(room)
             cave[i] = list(connections)
         return cave
+
+    def check_hazards(self):
+        if self.player == self.wumpus:
+            print("You were eaten by the Wumpus!")
+            self.alive = False
+        elif self.player in self.pits:
+            print("You fell into a bottomless pit!")
+            self.alive = False
+        elif self.player in self.bats:
+            print("Bats carried you to another room!")
+            self.player = random.choice([i for i in range(20) if i != self.player])
+            self.check_hazards()
