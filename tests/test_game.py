9af13639cff515f9124944a75c4c0aa347c3ad8c
eriklotysz13 <lotysze13@mycu.concord.edu>
2025-05-03 16:27:27 -0400

    fixed flake8 errors

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,7 @@
 import unittest
 from main import Cave
 
+
 class TestWumpusGame(unittest.TestCase):
     def test_cave_has_20_rooms(self):
         cave = Cave()
@@ -12,6 +13,7 @@ class TestWumpusGame(unittest.TestCase):
         next_room = cave.rooms[current][0]
         cave.move(next_room)
         self.assertEqual(cave.player, next_room)
+
 
 if __name__ == "__main__":
     unittest.main()
